c0e3280f8048c60dfb1cbdb3628c40d43fcc92ab
harshvardhan0709 <harshvardhan0709@gmail.com>
2021-07-29 00:05:18 +0530

    Fixed the workflow

--- src/surf/settings.py
+++ src/surf/settings.py
@@ -107,7 +107,18 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'github_actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            #'HOST': 'localhost',
+            'HOST': '127.0.0.1',
+            'PORT': '5432'
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
